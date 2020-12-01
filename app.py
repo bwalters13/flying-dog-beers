09995e63829572ebf32f98fe173539c33c7c71ee
bwalters13 <34872925+bwalters13@users.noreply.github.com>
2020-12-01 14:03:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input,Output
 import pandas as pd
 
 df = pd.read_csv('game1.csv')
@@ -38,12 +37,7 @@ app.layout = html.Div(children=[
     html.Div(id='my-output'),
 ])
 
-@app.callback(
-    Output(component_id='my-output', component_property='children'),
-    Input(component_id='my-input', component_property='value')
-)
-def update_output_div(input_value):
-    return 'Output: {}'.format(input_value)
+
 
 
 
